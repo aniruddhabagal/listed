8f37377736c5904bee139e58513e6645277e7384
Aniruddha Bagal <bagalaniruddha@gmail.com>
2023-05-06 13:17:25 +0530

    Added testimonials component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Navbar from "./components/navbar";
 import Hero from "./components/hero";
 import Features from "./components/features";
+import Testimonials from "./components/testimonials";
 import logoWhite from "./assets/logo-white.svg";
 import facebook from "./assets/icon-facebook.svg";
 import youtube from "./assets/icon-youtube.svg";
@@ -15,51 +16,8 @@ function App() {
       <Navbar />
       <Hero />
       <Features />
+      <Testimonials />
 
-      <section id="testimonials">
-        <div className="max-w-6xl px-5 mx-auto mt-32 text-center">
-          <h2 className="text-4xl font-bold text-center">
-            What{"'"}s Different About Listed?
-          </h2>
-
-          <div className="flex flex-col mt-24 md:flex-row md:space-x-6">
-            <div className="flex flex-col items-center p-6 space-y-6 rounded-lg bg-veryLightGray md:w-1/3">
-              <h5 className="text-lg font-bold">Anisha</h5>
-              <p className="text-sm text-darkGrayishBlue">
-                “Listed has supercharged our team’s workflow. The ability to
-                maintain visibility on larger milestones at all times keeps
-                everyone motivated.”
-              </p>
-            </div>
-
-            <div className="hidden flex-col items-center p-6 space-y-6 rounded-lg bg-veryLightGray md:flex md:w-1/3">
-              <h5 className="text-lg font-bold">Rahul</h5>
-              <p className="text-sm text-darkGrayishBlue">
-                “There is no more cross-channel confusion and everyone is much
-                more focused.”
-              </p>
-            </div>
-
-            <div className="hidden flex-col items-center p-6 space-y-6 rounded-lg bg-veryLightGray md:flex md:w-1/3">
-              <h5 className="text-lg font-bold">Avinash</h5>
-              <p className="text-sm text-darkGrayishBlue">
-                “Listed has supercharged our team’s workflow. The ability to
-                maintain visibility on larger milestones at all times keeps
-                everyone motivated.”
-              </p>
-            </div>
-          </div>
-
-          <div className="my-16">
-            <a
-              href="#"
-              className="p-3 px-6 pt-2 text-white bg-brightRed rounded-full baseline hover:bg-brightRedLight"
-            >
-              Dashboard
-            </a>
-          </div>
-        </div>
-      </section>
       <section id="cta" className="bg-brightRed">
         <div className="container flex flex-col items-center justify-between px-6 py-24 mx-auto space-y-12 md:py-12 md:flex-row md:space-y-0">
           <h2 className="text-5xl font-bold text-center text-white md:text-4xl md:max-w-xl md:text-left">
