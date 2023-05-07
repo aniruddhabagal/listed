83f7780b912a577f215fc17a4604e9f1bf29f04e
Aniruddha Bagal <bagalaniruddha@gmail.com>
2023-05-07 19:33:56 +0530

    Cleaned App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
 import "./App.css";
 import Home from "./pages/home";
-// import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
-
 import Dashboard from "./pages/dashboard/Dashboard";
 import Transactions from "./components/transactions/Transactions";
 import List from "./pages/list/List";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
-import Check from "./pages/Login.t";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      {/* <Route path="/signup" element={<SignUp />} /> */}
+
       <Route path="/login" element={<Login />} />
       <Route path="/dashboard" element={<Dashboard />}>
         <Route index element={<Dashboard />} />
@@ -23,9 +20,6 @@ function App() {
         <Route index element={<List />} />
         <Route path="transactions" element={<Transactions />} />
       </Route>
-      {/* <Route path="/check" element={<SignInForm />} /> */}
-
-      <Route path="/check" element={<Check />} />
     </Routes>
   );
 }
